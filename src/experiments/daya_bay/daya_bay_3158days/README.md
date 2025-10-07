9a999fc252fe36f244dec9c0b41591f8d27b3f38
Philipp Eller <peller.phys@gmail.com>
2025-10-07 09:48:28 +0200

    dayabay test

--- src/experiments/daya_bay/daya_bay_3158days/README.md
+++ src/experiments/daya_bay/daya_bay_3158days/README.md
@@ -3,10 +3,18 @@
 Data from supplemental files on https://arxiv.org/abs/2211.14988
 
 
-## Comparison Plot
-![Comparison](test.png)
-## Execution Details
-- **Hostname**: flippy
-- **User**: peller
-- **Date**: 2025-10-06 16:22:36
-- **git commit hash**: c499d2336105f31488f0796480f728bab0f1d5c2
+## Test output plots
+![Comparison](test_output/contours.png)
+![DataMC](test_output/datamc.png)
+## Meta Information
+- **repo_clean**: false
+- **exec_time**: 2.4554059505462646
+- **username**: peller
+- **repo**: /mnt/c/Users/peller/work/Newtrinos
+- **hostname**: flippy
+- **params**: (Δm²₂₁ = 7.53e-5, Δm²₃₁ = 0.0024752999999999997, δCP = 1.0, θ₁₂ = 0.5872523687443223, θ₁₃ = 0.1454258194533693, θ₂₃ = 0.8556288707523761)
+- **date**: 2025-10-07 09:47:05
+- **task**: scan
+- **vars_to_scan**: OrderedDict{Any, Any}(:θ₁₃ => 11, :Δm²₃₁ => 11)
+- **commit_hash**: 8b13ff2b23b385c04339621e438628ecd2bba30f
+- **priors**: (Δm²₂₁ = 7.53e-5, Δm²₃₁ = Uniform{Float64}(a=0.0023, b=0.0028), δCP = 1.0, θ₁₂ = 0.5872523687443223, θ₁₃ = Uniform{Float64}(a=0.13, b=0.16), θ₂₃ = 0.8556288707523761)
--- src/experiments/daya_bay/daya_bay_3158days/test.jl
+++ src/experiments/daya_bay/daya_bay_3158days/test.jl
@@ -38,11 +38,11 @@ priors = Newtrinos.get_priors(experiments)
 
 result = Newtrinos.profile(likelihood, priors, vars_to_scan, p, cache_dir="test")
 
-if !isdir("test")
-    mkdir("test")
+if !isdir("test_output")
+    mkdir("test_output")
 end
 
-FileIO.save("test/test.jld2", Dict("result" => result))
+FileIO.save("test_output/test.jld2", Dict("result" => result))
 
 converted = Newtrinos.NewtrinosResult(axes = (sin22theta13 = sin.(2*result.axes.θ₁₃).^2, Δm²₃₂ = result.axes.Δm²₃₁ .- p.Δm²₂₁), values=result.values);
 official_raw = CSV.read("DayaBay_DeltaChiSq_NO_3158days.txt", DataFrame, skipto=10, delim=" ", header=["sin22theta13", "Dm232", "Deltachi2"], ignorerepeated=true)
@@ -58,20 +58,20 @@ plot!(ax, official, levels=1 .- 2*ccdf(Normal(), 1:3), label="official", color=:
 ax.xlabel = "sin²2θ₁₃"
 ax.ylabel = "Δm²₃₂ (eV²)"
 axislegend(ax)
-save("test/contours.png", fig)
+save("test_output/contours.png", fig)
 
 bestfit = Newtrinos.bestfit(result)
 
 
 fig = experiments.dayabay.plot(bestfit)
-save("test/datamc.png", fig)
+save("test_output/datamc.png", fig)
 
 open("README.md", "w") do io
     write(io, "# DayaBay\n ## Resources\n")
     write(io, "Data from supplemental files on https://arxiv.org/abs/2211.14988\n\n")
     write(io, "\n## Test output plots\n")
-    write(io, "![Comparison](test/contours.png)\n")
-    write(io, "![DataMC](test/datamc.png)\n")
+    write(io, "![Comparison](test_output/contours.png)\n")
+    write(io, "![DataMC](test_output/datamc.png)\n")
     write(io, "## Meta Information\n")
     for (key, value) in result.meta
         write(io, "- **$(key)**: $(value)\n")
